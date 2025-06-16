e934f1e9dfd327f74a002db4ea1d73ba14e7f5e3
ace76765 <143945985+ace76765@users.noreply.github.com>
2025-06-17 04:43:05 +0530

    Add files via upload

---
+++ component.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Load header
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-container').innerHTML = data;
+        });
+    
+    // Load footer
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer-container').innerHTML = data;
+        });
+});
