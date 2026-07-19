04b283b0d270aa238a4776e45a413e9fe9a07453
utkarshranjan <utkarshranjan01@gmail.com>
2026-07-19 15:49:26 +0530

    Configure Next.js config for GitHub Pages export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/first-website',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
